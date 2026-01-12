d4e5cfe611ccfd5d6df3b8811337bca78cfdedb1
PlayboyMD90 <nidaganim2@gmail.com>
2026-01-12 20:47:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/PlayboyMD90/-/blob/main/imagejus/20260112_180822.jpg?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 PLAYBOY-MD Is Alive Now☹️*",
+BOT_OWNER: '94770614128',  // Replace with the owner's phone number
 
 
 
