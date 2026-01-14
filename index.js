7802f4962f6f8a2367dc45ec2cc26f655619c02c
PlayboyMD90 <nidaganim2@gmail.com>
2026-01-14 18:33:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const { commands, replyHandlers } = require('./command');
 const app = express();
 const port = process.env.PORT || 8000;
 
-const prefix = 'Play';
+const prefix = '.';
 const ownerNumber = ['94770614128'];
 const credsPath = path.join(__dirname, '/auth_info_baileys/creds.json');
 
